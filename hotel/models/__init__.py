00b0abf3d2df65df023ac3fbe278481eeb84551b
diosmedes780 <diosmedes003@gmail.com>
2023-07-11 16:57:24 -0400

    Migrando el modulo de hotel

--- hotel/models/__init__.py
+++ hotel/models/__init__.py
@@ -3,6 +3,6 @@
 # from . import account_move
 from . import hotel_folio
 from . import hotel_room
-# from . import hotel_services
+from . import hotel_services
 from . import product_product
 from . import res_company
